5a461a93bfa55933e94e4a2ac4313644453c5249
JDR <42478354+jdr81394@users.noreply.github.com>
2019-03-09 20:23:59 -0500

    l

--- FrontEnd_v1.1/app.js
+++ FrontEnd_v1.1/app.js
@@ -13,6 +13,28 @@ app.get('/sign_up', function(req,res){
   res.render('sign_up')
 })
 
+app.post('/sign_up'function(req,res){
+  //get data from form and add to campgrounds array
+var name = req.body.name;
+var price = req.body.price;
+var image = req.body.image;
+var desc = req.body.description;
+var author = {
+    id: req.user._id,
+    username: req.user.username
+};
+var newCampground = {name:name, price:price, image:image, description: desc, author:author};
+//Create a new campground and save to DB
+Campground.create(newCampground, function(err,newlyCreated){
+if(err){
+    console.log(err);
+} else {
+    //redirect to campgrounds
+    console.log(newlyCreated)
+    res.redirect("profile");
+}
+})
+
 
 app.listen(3000, process.env.IP, function(){
     console.log("Project has begun");
